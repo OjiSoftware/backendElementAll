3d73fac0d1c4ddf2544e5a55835152e33398d70c
joaquin <joaquinmaciel2910@gmail.com>
2026-02-04 11:36:26 +0100

    initial backend with express

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+    res.send("API funcionando 🚀");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
